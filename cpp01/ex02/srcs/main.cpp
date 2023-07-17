df7bdc00dd51776c68b7fe1a2e3d66548d7300bf
Noapoleon <noalegrand088@gmail.com>
2023-07-17 11:23:26 +0200

    added ex02 of cpp01

---
+++ cpp01/ex02/srcs/main.cpp
@@ -0,0 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: nlegrand <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/07/17 10:59:01 by nlegrand          #+#    #+#             */
+/*   Updated: 2023/07/17 11:18:43 by nlegrand         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+
+int	main(void)
+{
+	std::string	str			= "HI THIS IS BRAIN";
+	if (str.empty())
+		return (1);
+	std::string	*stringPTR	= &str;
+	std::string	&stringREF	= str;
+
+	std::cout << "Addr str -------> " << &str << std::endl;
+	std::cout << "Addr stringPTR -> " << stringPTR << std::endl;
+	std::cout << "Addr stringREF -> " << stringREF << std::endl << std::endl;
+	// Correction
+	//std::cout << "Addr stringREF -> " << &stringREF << std::endl << std::endl;
+
+	std::cout << "Value str -------> " << str << std::endl;
+	std::cout << "Value stringPTR -> " << *stringPTR << std::endl;
+	std::cout << "Value stringREF -> " << stringREF << std::endl;
+	// Correction
+	//std::cout << "Value stringREF -> " << *stringREF << std::endl;
+	return (0);
+}
