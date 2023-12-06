945a42746bd37fd218de367df4001c8db35aa03c
Noapoleon <noalegrand088@gmail.com>
2023-12-06 13:37:42 +0100

    tweaked constructor message cpp01/ex00

--- cpp01/ex00/srcs/Zombie.cpp
+++ cpp01/ex00/srcs/Zombie.cpp
@@ -6,25 +6,28 @@
 /*   By: nlegrand <nlegrand@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/16 05:15:58 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/07/18 14:02:06 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/12/06 13:18:10 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Zombie.hpp"
 
 // Constructors
-Zombie::Zombie() {}
+Zombie::Zombie()
+{
+	_name = "DefaultName";
+	std::cout << "Zombie '" << _name << "' default constructor called." <<  std::endl;
+}
 Zombie::Zombie(std::string name)
 {
-	this->_name = name;
-	std::cout << "Zombie '" << name << "' constructor called." <<  std::endl;
+	_name = name;
+	std::cout << "Zombie '" << _name << "' string constructor called." <<  std::endl;
 }
 
 // Destructors
 Zombie::~Zombie(void)
 {
-	std::cout << "Zombie '" << this->_name << "' destructor called."
-		<<  std::endl;
+	std::cout << "Zombie '" << _name << "' destructor called." <<  std::endl;
 }
 
 // Utils
