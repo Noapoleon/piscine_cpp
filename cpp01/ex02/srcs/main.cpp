5d04cffcf74c1df63a7c8cfd3b6005ffdad7cbaf
Noapoleon <noalegrand088@gmail.com>
2023-12-06 14:19:25 +0100

    changed prints to better follow subject

--- cpp01/ex02/srcs/main.cpp
+++ cpp01/ex02/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nlegrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/07/17 10:59:01 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/07/17 16:16:50 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/12/06 14:18:10 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,21 +15,15 @@
 int	main(void)
 {
 	std::string	str			= "HI THIS IS BRAIN";
-	if (str.empty())
-		return (1);
 	std::string	*stringPTR	= &str;
 	std::string	&stringREF	= str;
 
-	std::cout << "Addr str -------> " << &str << std::endl;
-	std::cout << "Addr stringPTR -> " << stringPTR << std::endl;
-	std::cout << "Addr stringREF -> " << &stringREF << std::endl << std::endl;
-	// Correction
-	//std::cout << "Addr stringREF -> " << &stringREF << std::endl << std::endl;
+	std::cout << "Addr str --------> " << &str << std::endl;
+	std::cout << "Value stringPTR -> " << stringPTR << std::endl;
+	std::cout << "Addr stringREF --> " << stringREF << std::endl << std::endl;
 
 	std::cout << "Value str -------> " << str << std::endl;
 	std::cout << "Value stringPTR -> " << *stringPTR << std::endl;
 	std::cout << "Value stringREF -> " << stringREF << std::endl;
-	// Correction
-	//std::cout << "Value stringREF -> " << *stringREF << std::endl;
 	return (0);
 }
