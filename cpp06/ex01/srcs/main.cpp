99941d57fabd5777fd1c7347fa8e0fa94cbf00d8
Noapoleon <noalegrand088@gmail.com>
2023-12-18 08:54:19 +0100

    added hex representation of raw pointer

--- cpp06/ex01/srcs/main.cpp
+++ cpp06/ex01/srcs/main.cpp
@@ -6,7 +6,7 @@
 /*   By: nlegrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 14:39:57 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/12/18 08:47:07 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/12/18 08:53:28 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,7 +25,9 @@ int	main(void)
 	std::cout << "Data before serialization: " << d.data << std::endl;
 
 	raw = Serializer::serialize(&d);
+	std::cout << "data pointer in Data* form: " << &d << std::endl;
 	std::cout << "Raw data pointer in uintptr_t form: " << raw << std::endl;
+	std::cout << "Raw data pointer in uintptr_t form (hex): " << std::hex << raw << std::dec << std::endl;
 
 	dd = Serializer::deserialize(raw);
 	std::cout << "Data after serialization: " << dd->data << std::endl;
