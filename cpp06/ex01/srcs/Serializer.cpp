5d85548512b17bf11ae83972a0008834505fd364
Noapoleon <noalegrand088@gmail.com>
2023-12-18 08:48:41 +0100

    added serializer

--- cpp06/ex01/srcs/Serializer.cpp
+++ cpp06/ex01/srcs/Serializer.cpp
@@ -6,8 +6,17 @@
 /*   By: nlegrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/17 17:16:05 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/12/17 17:16:13 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/12/18 08:47:29 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "Serializer.hpp"
+
+uintptr_t	Serializer::serialize(Data *ptr)
+{
+	return (reinterpret_cast<uintptr_t>(ptr));
+}
+Data*	Serializer::deserialize(uintptr_t raw)
+{
+	return (reinterpret_cast<Data *>(raw));
+}
--- cpp06/ex01/srcs/main.cpp
+++ cpp06/ex01/srcs/main.cpp
@@ -6,16 +6,31 @@
 /*   By: nlegrand <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/05 14:39:57 by nlegrand          #+#    #+#             */
-/*   Updated: 2023/12/17 17:16:26 by nlegrand         ###   ########.fr       */
+/*   Updated: 2023/12/18 08:47:07 by nlegrand         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include <iostream>
 #include <string>
+#include <stdint.h>
 #include "Serializer.hpp"
 
 int	main(void)
 {
-	std::cout << "Hello world" << std::endl;
+	Data		d;
+	uintptr_t	raw;
+	Data		*dd;
+
+	d.data = "Bonjour";
+	std::cout << "Data before serialization: " << d.data << std::endl;
+
+	raw = Serializer::serialize(&d);
+	std::cout << "Raw data pointer in uintptr_t form: " << raw << std::endl;
+
+	dd = Serializer::deserialize(raw);
+	std::cout << "Data after serialization: " << dd->data << std::endl;
+
+	//Serializer s; // Won't compile
+
 	return (0);
 }
